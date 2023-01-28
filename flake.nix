80b4e162a281e211fd25f087d8be6d505109d1d9
William Phetsinorath <william.phetsinorath@shikanime.studio>
2023-01-28 20:33:29 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = { nixpkgs, devenv, ... }@inputs: {
+    devShells = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.unix (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              pre-commit.hooks = {
+                actionlint.enable = true;
+                markdownlint.enable = true;
+                shellcheck.enable = true;
+                nixpkgs-fmt.enable = true;
+                statix.enable = true;
+                deadnix.enable = true;
+              };
+              packages = [
+                pkgs.nixpkgs-fmt
+              ];
+            }
+            {
+              pre-commit.hooks = {
+                eslint.enable = true;
+                prettier.enable = true;
+              };
+              packages = [
+                pkgs.nodejs
+              ];
+            }
+          ];
+        };
+      }
+    );
+  };
+}
